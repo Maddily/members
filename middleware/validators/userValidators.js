f3f1ce8a30ea48bd4df5b2cb7e8f0071b4a65294
Maddily <115709272+Maddily@users.noreply.github.com>
2026-09-24 10:00:06 +0300

    Create validateSignUp validator

---
+++ middleware/validators/userValidators.js
@@ -0,0 +1,27 @@
+const { body } = require("express-validator");
+const pool = require("../../db/pool");
+
+const validateSignUp = [
+  body("first-name").trim().notEmpty().withMessage("First name is required."),
+  body("last-name").trim().notEmpty().withMessage("Last name is required."),
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username is required.")
+    .custom(async (value) => {
+      const { rowCount } = await pool.query(
+        `SELECT * from users WHERE username = LOWER($1)`,
+        [value]
+      );
+      if (rowCount) throw new Error("Username already in use");
+    }),
+  body("password").notEmpty().withMessage("Password is required."),
+  body("confirm-password")
+    .notEmpty()
+    .withMessage("Confirm password is required.")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    }),
+];
+
+module.exports = { validateSignUp };
