771061d8ba4b18ad7162e236bc94b77a5ced54cc
Maddily <115709272+Maddily@users.noreply.github.com>
2026-09-25 21:41:50 +0300

    Use checkNotAuthenticated middleware
    If authenticated users visit /login or /sign-up
    routes, they're redirected to the homepage.

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -10,32 +10,39 @@ const {
   validateLogin,
 } = require("../middleware/validators/userValidators");
 const passport = require("passport");
+const { checkNotAuthenticated } = require("../middleware/auth");
 
 const authRouter = Router();
 
-authRouter.get("/sign-up", signUpGet);
-authRouter.post("/sign-up", validateSignUp, signUpPost);
-authRouter.get("/login", loginGet);
-authRouter.post("/login", validateLogin, loginPost, (req, res, next) => {
-  /**
-   * Pass a callback function instead of the options object
-   * to have control over redirection.
-   * `req.session.save(callback)` guarantees the session, including
-   * the flashed messages from the verify function, is fully written
-   * to the sessions table before `res.redirect` fires,
-   * fixing the race condition.
-   */
-  passport.authenticate("local", (error, user, info) => {
-    if (error) return next(error);
-
-    if (!user) return req.session.save(() => res.redirect("/login"));
-
-    req.logIn(user, (error) => {
+authRouter.get("/sign-up", checkNotAuthenticated, signUpGet);
+authRouter.post("/sign-up", checkNotAuthenticated, validateSignUp, signUpPost);
+authRouter.get("/login", checkNotAuthenticated, loginGet);
+authRouter.post(
+  "/login",
+  checkNotAuthenticated,
+  validateLogin,
+  loginPost,
+  (req, res, next) => {
+    /**
+     * Pass a callback function instead of the options object
+     * to have control over redirection.
+     * `req.session.save(callback)` guarantees the session, including
+     * the flashed messages from the verify function, is fully written
+     * to the sessions table before `res.redirect` fires,
+     * fixing the race condition.
+     */
+    passport.authenticate("local", (error, user, info) => {
       if (error) return next(error);
 
-      req.session.save(() => res.redirect("/"));
-    });
-  })(req, res, next);
-});
+      if (!user) return req.session.save(() => res.redirect("/login"));
+
+      req.logIn(user, (error) => {
+        if (error) return next(error);
+
+        req.session.save(() => res.redirect("/"));
+      });
+    })(req, res, next);
+  }
+);
 
 module.exports = authRouter;
